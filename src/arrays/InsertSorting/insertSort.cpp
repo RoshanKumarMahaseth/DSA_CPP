635a1f210a232b9325ecba309a3fe12ae7612496
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-07-28 19:38:36 +0530

    Selection Sort

---
+++ src/arrays/InsertSorting/insertSort.cpp
@@ -0,0 +1,31 @@
+//Sorting an array in asscending form by Insertion method...
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int arr[]={2,1,5,3,6,4};
+    int size = 6;
+    
+    //Outer loop
+    for(int i=0;i<size-1;i++){
+        int index = i;
+
+        //inner loop
+        for(int j=i+1; j<size; j++){
+            if(arr[j]<arr[index]){
+                index = j;
+            }
+        }
+        swap(arr[i],arr[index]);
+    }
+
+    
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
