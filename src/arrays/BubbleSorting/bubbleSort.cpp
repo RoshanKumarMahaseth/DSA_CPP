960c77d723b7f451af20ee66b114c5c468fd732b
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-07-30 11:15:06 +0530

    Reverse Bubble Sortin

--- src/arrays/BubbleSorting/bubbleSort.cpp
+++ src/arrays/BubbleSorting/bubbleSort.cpp
@@ -24,3 +24,30 @@ int main(){
 
     return 0;
 }
+
+
+
+
+//reverse bubble sorting 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,3,4,5};
+
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=size-1;i>0;i--){
+        for(int j=0;j<size-1;j++){
+            if(arr[j]<arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
