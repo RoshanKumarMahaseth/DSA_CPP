d090b479fad1cb65d4a9bd81f8208bf3a59f8fae
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-08-15 18:08:21 +0530

    leetcode

--- src/LeetcodeGG/leetcode_27/problem_27.cpp
+++ src/LeetcodeGG/leetcode_27/problem_27.cpp
@@ -1,30 +1,51 @@
 
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main(){
+// int main(){
 
-    int arr[]={0,1,2,2,3,0,4,2};
-    int size = 8;
-    int val = 2;
+//     int arr[]={0,1,2,2,3,0,4,2};
+//     int size = 8;
+//     int val = 2;
 
-    for(int i=0;i<size;i++){
-        if(arr[i]==val){
-            for(int j=i;j<size-1;j++){
-                arr[j] = arr[j+1];
+//     for(int i=0;i<size;i++){
+//         if(arr[i]==val){
+//             for(int j=i;j<size-1;j++){
+//                 arr[j] = arr[j+1];
+//             }
+//             size--;
+//             i--;
+//         }
+//     }
+
+
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i]<<" ";
+//     }
+
+//     cout<<endl;
+
+//     return 0;
+// }
+
+
+
+class Solution {
+public:
+    int removeElement(vector<int>& arr, int val) {
+        int size = arr.size();
+
+        for(int i=0;i<size;i++){
+            if(arr[i]==val){
+                for(int k=i;k<size-1;k++){
+                    arr[k] = arr[k+1];
+                }
+                size--;
+                i--;
             }
-            size--;
-            i--;
         }
+
+        return size;
     }
-
-
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
-    }
-
-    cout<<endl;
-
-    return 0;
-}
+};
