246f9ce91e527f7be912e0a48d31ffaab7f633c9
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-08-15 11:29:32 +0530

    TwoSum problem

---
+++ src/arrays/TwoSum/twosum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[]={2,7,11,15};
+    int size = 4;
+
+    int target=9;
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            if(arr[i]+arr[j]==target){
+                cout << i<<" "<<j<<endl;
+            }
+        }
+    }
+
+    return 0;
+}
