4c65803b64c2108ff6de392d55f08976dbbd1d42
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-08-21 11:58:26 +0530

    Kadane's Algorithm

---
+++ src/KadaneAlgo/kadanesalgorithm.cpp
@@ -0,0 +1,59 @@
+
+// //prefix sum 
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+// int main(){
+//     vector<int> arr = {6,4,5,-3,2,8};
+//     vector<int>prefix(arr.size());
+
+//     prefix[0] = arr[0];
+
+//     for(int i=1;i<arr.size();i++){
+//         prefix[i] = prefix[i-1]+arr[i];
+//     }
+
+//     for(int i=0;i<prefix.size();i++){
+//         cout<<prefix[i]<<" ";
+//     }
+//     cout<<endl;
+
+//     return 0;
+// }
+
+
+//suffix sum
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+// int main(){
+//     vector<int> arr = {6,4,5,-3,2,8};
+//     int n = arr.size();
+//     vector<int>suffix(n);
+
+//     suffix[n-1] = arr[n-1];
+//     for(int i=n-2;i>=0;i--){
+//         suffix[i]=suffix[i-2]+arr[i-1];
+//     }
+
+//     for(int i=0;i<suffix.size();i++){
+//         cout<<suffix[i]<<" ";
+//     }
+
+//     return 0;
+// }
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    
+}
+
+
