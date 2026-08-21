c2929ae0b02f31f0ffa532ea29b71c6374c4c401
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-08-21 20:15:01 +0530

    Trapping Rain Water

---
+++ src/Trapping Rain Water/trappingWater.cpp
@@ -0,0 +1,63 @@
+//two pointers approach
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int trap(vector<int>height){
+    int n = height.size();
+    int ans = 0;
+    int l = 0, r = n-1;
+    int lmax = 0 , rmax = 0;
+
+    while(l<r){
+        lmax = max(lmax,height[l]);
+        rmax = max(rmax,height[r]);
+
+        if(lmax<rmax){
+            ans += lmax - height[l];
+            l++;
+        }else{
+            ans += rmax - height[r];
+            r--;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int>height = {0,1,0,2,1,0,1,3,2,1,2};
+
+    cout<<trap(height)<<endl;
+
+    return 0;
+}
+
+
+
+
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l=0,r=n-1;
+        int lmax=0,rmax=0;
+        int ans=0;
+
+        while(l<r){
+            lmax = max(lmax,height[l]);
+            rmax = max(rmax,height[r]);
+
+            if(lmax<rmax){
+                ans += lmax-height[l];
+                l++;
+            }else{
+                ans += rmax - height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+};
