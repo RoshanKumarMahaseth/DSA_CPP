80ac9f6066aa272b346a67f9abb3219496583a4f
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-08-21 12:32:13 +0530

    Divide subarray sum

--- src/KadaneAlgo/kadanesalgorithm.cpp
+++ src/KadaneAlgo/kadanesalgorithm.cpp
@@ -47,13 +47,40 @@
 //     return 0;
 // }
 
+
+//Divide array in 3 subarray with equal sum
 #include <iostream>
 #include <vector>
+#include <climits>
 #include <algorithm>
 using namespace std;
 
-int main(){
-    
+bool divide(vector<int>arr){
+    int prefix=0,total_sum=0,n=arr.size();
+    //total sum
+
+    for(int i=0;i<n;i++){
+        total_sum+=arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        prefix+=arr[i];
+        if(total_sum == 2*prefix){
+            return 1;
+        }
+    }
+    return 0;
 }
+int main(){
+    int n;
+    cout<<"enter the size of array: ";
+    cin>>n;
+    vector<int>v(n);
+    cout<<"enter the element in array: ";
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
 
+    cout<<divide(v);
 
+}
