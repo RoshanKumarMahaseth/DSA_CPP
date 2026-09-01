2b75fc98134d1d4855f16af282d946da51d818d4
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-09-01 20:16:53 +0530

    2D arrays

---
+++ src/2Darrays/2Darray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+void printcol(int arr[][4],int row,int col){
+    for(int j=0;j<col;j++){
+        for(int i=0;i<row;i++){
+            cout<<arr[i][j]<<" ";
+        }
+    }
+}
+int main(){
+
+
+    int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+
+    for(int row=0;row<3;row++){
+        for(int col=0;col<4;col++){
+            cout<<arr[row][col]<<" ";
+        }
+    }
+    printcol(arr,3,4);
+
+    return 0;
+}
