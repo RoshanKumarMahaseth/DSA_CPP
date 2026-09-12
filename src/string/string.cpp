609841ca8f81f259fcc7ecc1ebf087ba9abcb040
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-09-12 14:39:35 +0530

    Strings

---
+++ src/string/string.cpp
@@ -0,0 +1,40 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     char arr[]={'a','p','p','l','e'};
+//     int size = sizeof(arr)/sizeof(arr[0]);
+
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i];
+//     }
+//     cout<<endl;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     char arr[1000];
+
+//     int size;
+//     cout<<"size: ";
+//     cin>>size;
+
+//     cout<<"enter array: ";
+//     for(int i=0;i<size;i++){
+//         cin>>arr[i];
+//     }
+
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i];
+//     }
+//     cout<<endl;
+
+//     return 0;
+// }
+
+
