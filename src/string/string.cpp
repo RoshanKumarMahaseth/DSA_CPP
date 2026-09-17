b3d88bcd3ddac3b9bfa039f15e5807ef533cf60f
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-09-17 13:13:44 +0530

    String

--- src/string/string.cpp
+++ src/string/string.cpp
@@ -37,12 +37,117 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     string s = "roshan";
+//     cout<<s<<endl;
+
+//     string s1 = "rohit";
+//     string s2 = "class";
+    
+//     string s3 = s1+s2;
+
+//     cout<<s3<<endl;
+
+//     s1.push_back('x');
+//     cout<<s1<<endl;
+
+//     s2.pop_back();
+//     cout<<s2<<endl;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     string s = "roshan is a part time gamer";
+//     cout<<s<<endl;
+
+
+//     string r = "roshan study \"dsa daily\" ";
+//     cout<<r<<endl;
+
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     string s;
+//     getline(cin,s);
+//     cout<<s<<endl;
+//     cout<<s.size();
+
+
+//     string s1;
+//     cin>>s1;
+//     cout<<s1<<endl;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     // string s1 = "roshan", s2 = " diamond juice";
+//     // s1 = s1+"p";
+//     // cout<<s1;
+
+//     string s = "\\0";
+//     cout<<s<<endl;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     string s = "Roshan";
+//     int st=0,end=s.size()-1;
+//     while(st<end){
+//         swap(s[st],s[end]);
+//         st++,end--;
+//     }
+
+//     cout<<s<<endl;
+// }
+
+
 #include <iostream>
 using namespace std;
 
 int main(){
     string s = "roshan";
-    cout<<s<<endl;
+    int size=0;
+    while(s[size]!='\0'){
+        size++;
+    }
 
-    return 0;
+    cout<<size<<endl;
+
+    string s2 = "namam";
+    int st=0,end=s2.size()-1;
+
+    while(st<end){
+        if(s2[st]!=s2[end]){
+            cout<<"not a palidrome";
+            return 0;
+        }
+        st++,end--;
+    }
+    cout<<"its a palidrome";
+    
+    
 }
---
+++ src/string/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     string s = "Roshan";
+//     int st=0,end=s.size()-1;
+//     while(st<end){
+//         swap(s[st],s[end]);
+//         st++,end--;
+//     }
+
+//     cout<<s<<endl;
+// }
