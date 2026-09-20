7d9c590303870d249fe9c6bd4ac3d135f15bc10c
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-09-20 16:52:27 +0530

    Pangram problem

--- src/string/check_pangram.cpp
+++ src/string/check_pangram.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 int main(){
-    string sentence = "thequickbrownfoxjumpsoverthelazydog";
-
-    vector<bool>alpha(26,0);
-    for(int i=0;i<sentence.size();i++){
-        alpha[sentence[i]-'a']=1;
+    vector<bool> arr(26,0);
+    string s = "thequickbrownfoxjumpsoverthelazydog";
+    for(int i=0;i<s.size();i++){
+        arr[s[i]-'a']=1;
     }
+
     for(int i=0;i<26;i++){
-        if(alpha[i]==0){
+        if(arr[i]==0){
             return 0;
         }
     }
-
     return 1;
 }
