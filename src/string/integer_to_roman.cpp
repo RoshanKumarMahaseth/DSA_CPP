160535f45b50a5b28a17626cc09c6ac2b9312f4f
RoshanKumarMahaseth <kumarmahasethroshan@gmail.com>
2026-09-21 15:14:12 +0530

    Integer to roman

--- src/string/integer_to_roman.cpp
+++ src/string/integer_to_roman.cpp
@@ -1,7 +1,28 @@
-
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    
+string intToRoman(int num) {
+
+    static vector<int> val{1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+    vector<string> sym{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+    string result = "";
+    for(int i = 0; i < 13; i++) {
+        if(num == 0) {
+            break;
+        }
+        int times = num / val[i];
+        while(times--) {
+            result += sym[i];
+        }
+        num = num % val[i];
+    }
+    return result;
+}
+int main() {
+    int num = 1994;
+    cout << intToRoman(num);
+    return 0;
 }
